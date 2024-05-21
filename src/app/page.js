1343a2b18e517c2cd95c3dbbdec3f946c251e024
dvlpralamin <alaminhowladerofficial@gmail.com>
2024-05-21 16:40:56 +0600

    [feat]:responsive typography & grid

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 "use client"
-import { Box, Container, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { Box, Container, CssBaseline, Grid, ThemeProvider, Typography } from "@mui/material";
 import theme from "./theme/theme";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const containerRef = useRef();
-  const contentRef = useRef()
-  const [width, setWidth] = useState(0)
-  const [contentWidth, setContentWidth] = useState(0)
+  const contentRef = useRef();
+  const [width, setWidth] = useState(0);
+  const [contentWidth, setContentWidth] = useState(0);
   useEffect(() => {
     const calcWidth = () => {
       if (containerRef.current) {
@@ -28,13 +28,13 @@ export default function Home() {
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <Container ref={containerRef} sx={{ background: "#C3CF8B" }}>
-          <Box ref={contentRef} sx={{ background: "gray", height: "100vh", pt: 5 }}>
-            <Typography sx={{ fontSize: 40, fontWeight: 600, textAlign: "center" }}>⬅️Container width:{width}px➡️</Typography>
-            <Typography sx={{ fontSize: 40, fontWeight: 600, textAlign: "center" }}>⬅️Content width:{contentWidth}px➡️</Typography>
+          <Box ref={contentRef} sx={{ background: "gray", minHeight: "100vh", pt: 5 }}>
+            <Typography sx={{ fontSize: { xl: 50, lg: 45, md: 40, sm: 30, xs: 20, xxs: 15 }, fontWeight: 600, textAlign: "center" }}>⬅️Container width:{width}px➡️</Typography>
+            <Typography sx={{ fontSize: { xl: 50, lg: 45, md: 40, sm: 30, xs: 20, xxs: 15 }, fontWeight: 600, textAlign: "center" }}>⬅️Content width:{contentWidth}px➡️</Typography>
             <Box sx={{
               position: "relative",
-
-              fontSize: { md: 40, sm: 30, xs: 20, xxs: 15 },
+              height: 100,
+              fontSize: { xl: 50, lg: 45, md: 40, sm: 30, xs: 20, xxs: 15 },
               '&:before': {
                 position: "absolute",
                 top: 0,
@@ -44,9 +44,18 @@ export default function Home() {
                 content: { xl: `"Screen size xl"`, lg: `"Screen size lg"`, md: `"Screen size md"`, sm: `"Screen size sm"`, xs: `"Screen size xs"`, xxs: `"Screen size xxs"` }
               }
             }}></Box>
+            <Grid container spacing={3}>
+              {
+                [...new Array(12)].map((_, index) => (
+                  <Grid item lg={3} md={4} sm={6} xxs={12} key={index}>
+                    <Box sx={{ height: 400, background: "#ccc" }}></Box>
+                  </Grid>
+                ))
+              }
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
+        </Container >
+      </ThemeProvider >
     </>
   );
 }
